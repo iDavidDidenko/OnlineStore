94ec4091f78ba99211bde38752136e52d0a08fb4
David Did <daviddid@Davids-MacBook-Air.local>
2024-01-15 02:46:26 +0200

    cart_build_hands_on_v2

--- obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e54577b3121e8bcd376b630c62550b2b93b0df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364776428bdd5f422ac7f6259e2e1cd72ae0c40e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
